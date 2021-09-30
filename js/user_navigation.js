68aba55084087c3696c1f3613474bf4ebc2b60f6
dchosnek <dchosnek@cisco.com>
2021-09-30 16:28:20 -0500

    last of Thursday feedback updates

--- js/user_navigation.js
+++ js/user_navigation.js
@@ -16,3 +16,35 @@ if (Object.keys(params).includes('pod')) {
         element.appendChild(text);
     });
 }
+
+let key_input = document.getElementById('api_key');
+if (key_input) {
+    if (localStorage.getItem('api_key') !== null ) {
+        let placeholder = document.getElementById('api_key_placeholder')
+        placeholder.innerText = localStorage.getItem('api_key');
+        placeholder.parentElement.parentElement.classList.remove("hidden");
+        document.getElementById('api_key').parentElement.classList.add("hidden");
+    }
+}
+function set_api_key(val) {
+    let trimmed_value = document.getElementById(val).value.trim();
+    console.log(trimmed_value);
+    if (trimmed_value == null || trimmed_value === "") {
+        return
+    }
+    localStorage.setItem('api_key', trimmed_value);
+    let placeholder = document.getElementById('api_key_placeholder')
+    placeholder.innerText = trimmed_value;
+    placeholder.parentElement.parentElement.classList.remove("hidden");
+    document.getElementById(val).parentElement.classList.add("hidden");
+    location.reload();
+}
+
+function change_api_key(val) {
+    let key_input = document.getElementById(val);
+    key_input.value = "";
+    let placeholder = document.getElementById('api_key_placeholder')
+    placeholder.innerText = '';
+    placeholder.parentElement.parentElement.classList.add("hidden");
+    document.getElementById(val).parentElement.classList.remove("hidden");
+}
