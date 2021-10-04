9321dade1dd83c4c056d3d5dc592097539438980
Brandon Beck <beckbran@gmail.com>
2021-10-04 11:30:08 -0500

    fixed css and js

--- js/user_navigation.js
+++ js/user_navigation.js
@@ -1,11 +1,11 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
+let root = document.documentElement;
 if (Object.keys(params).includes('pod')) {
     localStorage.setItem('pod', params.pod);
     root.style.setProperty('--pod', `${params.pod}`);
 } else {
-    let root = document.documentElement;
     if (localStorage.getItem('pod') === null) {
         root.style.setProperty('--showUserWarnings', 'block');
     }
